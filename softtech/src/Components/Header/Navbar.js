aee15da9fbae874df1a98c224e248685dbe444b5
gauravpandey9129 <gaurav912959@gmail.com>
2024-04-11 14:12:52 +0530

    implementingContextApiForModal

--- softtech/src/Components/Header/Navbar.js
+++ softtech/src/Components/Header/Navbar.js
@@ -3,24 +3,25 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
 import FormModal from './FormModal';
+import { useBlogContext } from '../../ContextApi/Context';
  
 
 
 export default function TopNavbar() {
     
-    const [modalStatus,setModalStatus]=useState(false)
+     const {modalStatus,setModalStatus}=useBlogContext()
 
-    const handleClose=()=>{
-          setModalStatus(false)
-    }
+     const handleClose=()=>{
+      setModalStatus(true)
+     }
 
   return (
     <div>
      <Navbar expand='lg' className='bg-info' >
         <Container>
         <Navbar.Brand className=' col text-center fw-bold ' href="#home">Blog-Website</Navbar.Brand><br/>
-        <Button type='button' onClick={()=>setModalStatus(true)} variant='primary' className='text-light' >Add a new Blog</Button>
-        {modalStatus?<FormModal  handleClose={handleClose} modalStatus={modalStatus}  />:null}
+        <Button type='button' onClick={handleClose } variant='primary' className='text-light' >Add a new Blog</Button>
+        {modalStatus?<FormModal   />:null}
         </Container>
      </Navbar>
 
