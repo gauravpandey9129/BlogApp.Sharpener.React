f320471ec7620209a06ffa17db1f28447fae4c9f
gauravpandey9129 <gaurav912959@gmail.com>
2024-04-11 13:03:04 +0530

    importedNavbarInApp.js

---
+++ softtech/src/App.js
@@ -0,0 +1,16 @@
+ import React from "react";
+import TopNavbar from "./Components/Header/Navbar";
+  
+
+function App() {
+  return (
+    <>
+    <TopNavbar/>
+    
+    
+    
+    </>
+  );
+}
+
+export default App;
