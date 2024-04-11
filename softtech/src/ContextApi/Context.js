47d5b72deec714b5aa8e0456497edc8f679ecf58
gauravpandey9129 <gaurav912959@gmail.com>
2024-04-11 14:41:24 +0530

    CreatedBlogContext

--- softtech/src/ContextApi/Context.js
+++ softtech/src/ContextApi/Context.js
@@ -7,12 +7,14 @@ export const useBlogContext=()=>useContext(BlogContext);
 
 export const BlogContextProvider=({children})=>{
  
-    const [modalStatus,setModalStatus]=useState(false)
+    const [modalStatus,setModalStatus]=useState(false);
+    const [blogs,setBlogs]=useState([])
+
      
 
     return(
    
-        <BlogContext.Provider  value={{modalStatus,setModalStatus}} >
+        <BlogContext.Provider  value={{modalStatus,setModalStatus,blogs,setBlogs}} >
            {children}
         </BlogContext.Provider>
 
