552f2840f7a32adf3bd35bc5207ed62890a5b631
gauravpandey9129 <gaurav912959@gmail.com>
2024-04-11 17:17:54 +0530

    ShowingBlogsFromContextApi

--- softtech/src/Components/Blog/Blogs.js
+++ softtech/src/Components/Blog/Blogs.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { useBlogContext } from '../../ContextApi/Context';
@@ -7,20 +7,29 @@ export default function Blogs() {
 
     const {blogs,setBlogs}=useBlogContext()
 
+    const handleOnDelete=(id)=>{
+      setBlogs(blogs.filter((elements)=> elements.id!==id )) ;
+        
+
+    }
+     
+    
+
   return (
     <>
-
-       { blogs.map(elements=> <Card key={elements.id} style={{ width: '18rem' }}>
+     <div className='row'>
+       { blogs.map(elements=> <Card  className='mt-5 ms-5 col-sm-6'  key={elements.id} style={{ width: '18rem' }}>
         <Card.Img variant="top" src={elements.img} />
         <Card.Body>
             <Card.Title> {elements.title} </Card.Title>
             <Card.Text>
-             {elements.description}
+             {elements.decription}
             </Card.Text>
-            <Button variant="primary">Delete</Button>
-            <Button variant="primary">Edit</Button>
+            <Button onClick={() => handleOnDelete(elements.id)} variant="primary">Delete</Button>
+            <Button  className='ms-3'  variant="primary">Edit</Button>
         </Card.Body>
         </Card>)}
+        </div>
 
     </>
   )
